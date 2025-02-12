d05b37b6333db708967ea45c62232af92acf006a
eeoooue <pdmoorhouse@gmail.com>
2025-02-12 23:58:19 +0000

    scaffolded a system for reading the state of the CubePuzzle

--- Solution/LibNetCube/CubePuzzle.cs
+++ Solution/LibNetCube/CubePuzzle.cs
@@ -2,10 +2,62 @@
 {
     public class CubePuzzle
     {
+        public const int FaceWidth = 3;
+
+        private int[,] TopFace = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+        private int[,] BottomFace = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+
+        private int[,] LeftFace = { { 3, 3, 3 }, { 3, 3, 3 }, { 3, 3, 3 } };
+        private int[,] RightFace = { { 4, 4, 4 }, { 4, 4, 4 }, { 4, 4, 4 } };
+
+        private int[,] FrontFace = { { 2, 2, 2 }, { 2, 2, 2 }, { 2, 2, 2 } };
+        private int[,] BackFace = { { 5, 5, 5 }, { 5, 5, 5 }, { 5, 5, 5 } };
 
         public CubeState GetState()
         {
-            throw new NotImplementedException();
+            List<int[,]> reads = new List<int[,]>();
+            List<CubeFace> faces = CubeState.GetFaceNames();
+            foreach (CubeFace face in faces)
+            {
+                int[,] read = ReadFace(face);
+                reads.Add(read);
+            }
+
+            return new CubeState(reads);
+        }
+
+        public int[,] ReadFace(CubeFace face)
+        {
+            int[,] result = new int[FaceWidth, FaceWidth];
+            int[,] source = GetFaceRef(face);
+            for(int i=0; i<FaceWidth; i++)
+            {
+                for(int j=0; j<FaceWidth; j++)
+                {
+                    result[i, j] = source[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        private int[,] GetFaceRef(CubeFace face)
+        {
+            switch (face)
+            {
+                case CubeFace.Back:
+                    return BackFace;
+                case CubeFace.Bottom:
+                    return BottomFace;
+                case CubeFace.Front:
+                    return FrontFace;
+                case CubeFace.Left:
+                    return LeftFace;
+                case CubeFace.Right:
+                    return RightFace;
+                default:
+                    return TopFace;
+            }
         }
 
         private void RotateTopFaceClockwise()
