387d9949bc1574fecf1a35fd146d20019515febf
eeoooue <pdmoorhouse@gmail.com>
2025-02-13 00:22:35 +0000

    scaffolded a turn top layer mechanic

--- Solution/LibNetCube/CubePuzzle.cs
+++ Solution/LibNetCube/CubePuzzle.cs
@@ -13,6 +13,11 @@
         private int[,] FrontFace = { { 2, 2, 2 }, { 2, 2, 2 }, { 2, 2, 2 } };
         private int[,] BackFace = { { 5, 5, 5 }, { 5, 5, 5 }, { 5, 5, 5 } };
 
+        public CubePuzzle()
+        {
+            RotateTopFaceClockwise();
+        }
+
         public CubeState GetState()
         {
             List<int[,]> reads = new List<int[,]>();
@@ -62,7 +67,83 @@
 
         private void RotateTopFaceClockwise()
         {
+            ReplaceFaceWithClockwiseRotation(CubeFace.Top);
+            RotateTopRowsLeftwise(CubeFace.Front, CubeFace.Right, CubeFace.Back, CubeFace.Left);
+        }
 
+        private void ReplaceFaceWithClockwiseRotation(CubeFace face)
+        {
+            CubeState previous = GetState();
+            int[,] prevTopFace = previous.GetFace(face);
+            int[,] newTopFace = RotateFaceClockwise(prevTopFace);
+            ReplaceEntireFace(face, newTopFace);
+        }
+
+        private void ReplaceEntireFace(CubeFace face, int[,] values)
+        {
+            int[,] destination = GetFaceRef(face);
+            for (int i = 0; i < FaceWidth; i++)
+            {
+                for (int j = 0; j < FaceWidth; j++)
+                {
+                    destination[i, j] = values[i, j];
+                }
+            }
+        }
+
+        private void RotateTopRowsLeftwise(CubeFace a, CubeFace b, CubeFace c, CubeFace d)
+        {
+            int[] nextA = ReadTopRowOfFace(b);
+            int[] nextB = ReadTopRowOfFace(c);
+            int[] nextC = ReadTopRowOfFace(d);
+            int[] nextD = ReadTopRowOfFace(a);
+            ReplaceTopRowOfFace(a, nextA);
+            ReplaceTopRowOfFace(b, nextB);
+            ReplaceTopRowOfFace(c, nextC);
+            ReplaceTopRowOfFace(d, nextD);
+        }
+
+        private void ReplaceTopRowOfFace(CubeFace face, int[] values)
+        {
+            int[,] destination = GetFaceRef(face);
+            for (int j = 0; j < FaceWidth; j++)
+            {
+                destination[0, j] = values[j];
+            }
+        }
+
+        private int[] ReadTopRowOfFace(CubeFace face)
+        {
+            int[] result = new int[FaceWidth];
+            int[,] source = GetFaceRef(face);
+            for (int j = 0; j < FaceWidth; j++)
+            {
+                result[j] = source[0, j];
+            }
+
+            return result;
+        }
+
+        public static int[,] RotateFaceClockwise(int[,] values)
+        {
+            int[,] result = new int[3, 3];
+
+            // corners 
+            result[0, 0] = values[2, 0];
+            result[0, 2] = values[0, 0];
+            result[2, 2] = values[0, 2];
+            result[2, 0] = values[2, 2];
+
+            // center point
+            result[1, 1] = values[1, 1];
+
+            // edge pieces
+            result[0, 1] = values[1, 0];
+            result[1, 2] = values[0, 1];
+            result[2, 1] = values[1, 2];
+            result[1, 0] = values[2, 1];
+
+            return result;
         }
 
         private void RollForwards()
