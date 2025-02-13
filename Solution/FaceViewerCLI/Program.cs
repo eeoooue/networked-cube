7784682973fd82ee277a95cf93359a86996d6511
eeoooue <80429519+eeoooue@users.noreply.github.com>
2025-02-13 09:59:33 +0000

    scaffolded face viewer

---
+++ Solution/FaceViewerCLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using LibNetCube;
+
+namespace FaceViewerCLI
+{
+    internal class Program
+    {
+        private static FacePresenter Presenter = new FacePresenter();
+
+        static void Main(string[] args)
+        {
+            CubePuzzle puzzle = new CubePuzzle();
+            puzzle.PerformMove('U');
+
+
+            CubeState state = puzzle.GetState();
+            Presenter.PresentCube(state);
+        }
+    }
+}
