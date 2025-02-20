749f9cd8c79df15b4fb2cbd79af7dedb4b804a4d
eeoooue <80429519+eeoooue@users.noreply.github.com>
2025-02-20 10:39:15 +0000

    Update Program.cs

--- Solution/CubeProxy/Program.cs
+++ Solution/CubeProxy/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// Will be multithreaded in using Threadpool threads
+        /// </summary>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PuzzleCubeServer.StartServer();
         }
+
+
+
     }
 }
