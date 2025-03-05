9e196cb21ad716c3dca459a3c6205cecfa39a0a9
eeoooue <pdmoorhouse@gmail.com>
2025-03-05 14:32:01 +0000

    scaffolded 26-piece wrapper classes

---
+++ Solution/LibNetCube/PuzzlePiece.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibNetCube
+{
+    internal abstract class PuzzlePiece
+    {
+        public abstract int GetPieceFacing(CubeFace face);
+
+        public abstract void SetPieceFacing(CubeFace face, int value);
+    }
+}
