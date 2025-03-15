8438c8af3f622bb3cf088a2e6ae98010d1558158
eeoooue <pdmoorhouse@gmail.com>
2025-03-15 23:10:58 +0000

    scaffolded pochman wrapper

---
+++ Solution/LibNetCube/PochmanWrapper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibNetCube
+{
+    internal class PochmanWrapper
+    {
+        private CubeState State;
+
+        public PochmanWrapper(CubeState state)
+        {
+            State = state;
+        }
+
+        public int GetPiece(char x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetPiece(CubeFace face, int i, int j)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPiece(char x, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPiece(CubeFace face, int i, int j, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetPieceLocation(CubeFace face, int i, int j)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetPieceValues(char x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPieceValues(List<char> positions, List<int> values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
