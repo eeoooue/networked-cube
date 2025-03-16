bc6e188a518da4b9e045bccfd0277a1054a61871
eeoooue <pdmoorhouse@gmail.com>
2025-03-16 02:48:47 +0000

    scaffolded ScrambleAlgorithm as an open element for contributors

---
+++ Solution/LibNetCube/ScrambleAlgorithm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibNetCube
+{
+    public static class ScrambleAlgorithm
+    {
+        private static Random Randomizer = new Random();
+
+        public static List<CubeMove> GenerateScramble()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
