837a7caec99abc435d7403fb1722d90d4ef6e5eb
eeoooue <pdmoorhouse@gmail.com>
2025-03-16 02:48:39 +0000

    scaffolded MoveParser as an open element for contributors

---
+++ Solution/LibNetCube/MoveParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibNetCube
+{
+    public static class MoveParser
+    {
+        public static CubeMove ParseMove(string move)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
