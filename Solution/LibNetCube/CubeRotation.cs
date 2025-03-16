f74e156348198cc510bdf48e2d092f85f33e877d
eeoooue <pdmoorhouse@gmail.com>
2025-03-16 00:03:40 +0000

    scaffolded a CubeRotation method

---
+++ Solution/LibNetCube/CubeRotation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibNetCube
+{
+    internal static class CubeRotation
+    {
+
+
+        public static void RotateTopFaceClockwise(CubeState state)
+        {
+            PochmanWrapper wrapper = new PochmanWrapper(state);
+
+            StringBuilder sources = new StringBuilder();
+            sources.Append("AaBbCcDd"); // original top ring
+            sources.Append("QqR"); // left row
+            sources.Append("EeF"); // front row
+            sources.Append("IiJ"); // right row
+            sources.Append("MmN"); // back right
+
+            StringBuilder destinations = new StringBuilder();
+            sources.Append("BbCcDdAa"); // rotated top ring
+            sources.Append("MmN"); // left row becomes back row
+            sources.Append("QqR"); // front row becomes left row
+            sources.Append("EeF"); // right row becomes front row
+            sources.Append("IiJ"); // back row becomes right row
+
+            wrapper.ApplyMove(sources.ToString(), destinations.ToString());
+        }
+
+
+    }
+}
