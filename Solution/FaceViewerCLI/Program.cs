13c5e6501c0395c3e3218bb9b49770b25eebd5e2
eeoooue <pdmoorhouse@gmail.com>
2025-03-21 19:29:51 +0000

    simplified Program.cs by using strategy objects

--- Solution/FaceViewerCLI/Program.cs
+++ Solution/FaceViewerCLI/Program.cs
@@ -2,6 +2,8 @@
 using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
+using FaceViewerCLI.GetCubeStrategies;
+using FaceViewerCLI.PerformMoveStrategies;
 using LibNetCube;
 
 namespace FaceViewerCLI
@@ -9,59 +11,37 @@ namespace FaceViewerCLI
     internal class Program
     {
         private static FacePresenter Presenter = new FacePresenter();
+        private static CubeState CubeState = new CubeState(new CubePuzzle().GetState());
+
+        private static IPerformMoveStrategy PerformMoveStrategy = new MoveViaSocketStrategy();
+        private static IGetCubeStrategy GetCubeStrategy = new GetCubeViaSocketStrategy();
 
         static void Main(string[] args)
         {
             while (true)
             {
+                if (GetCubeStrategy.GetCube() is CubeState state)
+                {
+                    CubeState = state;
+                }
+                else
+                {
+                    Console.WriteLine("Lost connection to server.");
+                    Thread.Sleep(500);
+                    Console.WriteLine("Attempting to reconnect...");
+                    continue;
+                }
+
+                Presenter.PresentCube(CubeState);
+
                 Console.WriteLine("Enter a move to be performed...");
                 string? message = Console.ReadLine();
 
                 if (message != null)
                 {
-                    SendMoveRequest(message);
+                    PerformMoveStrategy.PerformMove(message);
                 }
             }
         }
-
-        static void SendMoveRequest(string message)
-        {
-            TcpClient tcpClient = new TcpClient();
-            tcpClient.Connect("127.0.0.1", 5000);
-
-            NetworkStream nStream = tcpClient.GetStream();
-
-            if (message != null)
-            {
-                byte[] request = Serialize(message);
-                nStream.Write(request, 0, request.Length);
-                // TODO: Read response from stream and display to user
-                byte[] received = ReadFromStream(nStream);
-
-                CubeState state = new CubeState(received);
-                Presenter.PresentCube(state);
-            }
-        }
-
-        static byte[] ReadFromStream(NetworkStream stream)
-        {
-            int messageLength = stream.ReadByte();
-            byte[] messageBytes = new byte[messageLength];
-            stream.Read(messageBytes, 0, messageLength);
-            return messageBytes;
-        }
-
-        static byte[] Serialize(string request)
-        {
-            byte[] responseBytes = Encoding.ASCII.GetBytes(request);
-            byte responseLength = (byte)responseBytes.Length;
-
-            byte[] rawData = new byte[responseLength + 1];
-            rawData[0] = responseLength;
-            responseBytes.CopyTo(rawData, 1);
-            return rawData;
-        }
-
-
     }
 }
