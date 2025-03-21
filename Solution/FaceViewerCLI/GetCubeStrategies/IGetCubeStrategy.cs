680e70408062fce5ca544a5e48c1bca2cb2b3abc
eeoooue <pdmoorhouse@gmail.com>
2025-03-21 19:29:09 +0000

    added strategy pattern interfaces for performing moves & getting cubes via different protocols

---
+++ Solution/FaceViewerCLI/GetCubeStrategies/IGetCubeStrategy.cs
@@ -0,0 +1,14 @@
+﻿using LibNetCube;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceViewerCLI
+{
+    internal interface IGetCubeStrategy
+    {
+        public CubeState? GetCube();
+    }
+}
