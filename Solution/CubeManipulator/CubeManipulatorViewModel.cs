4631cb43c53adf66163bcebb7a96c5154da200c6
Nikita <zazoozazoozaza@gmail.com>
2025-03-28 22:10:18 +0300

    CubeManipulator MVVM Realisation
    Realise MVVM:
    Model - MoveViaApiStrategy from LibCubeIntegration,
    ViewModel - CubeManipulatorViewModel.
    RelayCommand class - ICommand Realisation
    View - Binding MoveCommand with CommandParameter

---
+++ Solution/CubeManipulator/CubeManipulatorViewModel.cs
@@ -0,0 +1,26 @@
+﻿using LibCubeIntegration.PerformMoveStrategies;
+
+namespace CubeManipulator
+{
+    public class CubeManipulatorViewModel
+    {
+        //full path MoveViaApi
+        LibCubeIntegration.PerformMoveStrategies.MoveViaApiStrategy moveStrategy = new MoveViaApiStrategy();
+
+        private RelayCommand moveCommand;
+        public RelayCommand MoveCommand //Get command result
+        {
+            get
+            {
+                return moveCommand ??
+                    (moveCommand = new RelayCommand(obj =>
+                    {
+                        string parsedObj = obj is string ? (string)obj : string.Empty;
+
+                        Task<bool> task = moveStrategy.PerformMoveAsync(parsedObj);
+                    }));
+            }
+        }
+
+    }
+}
