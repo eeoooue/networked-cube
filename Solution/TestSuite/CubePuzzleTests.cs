dbf8f3dbb6105353062eb3f6cc0e8a36f89281cd
MrBytesizedCode <146436021+W1ll-Gale@users.noreply.github.com>
2025-03-28 15:40:14 +0000

    test: added UnitTest for reset functionality when multiple moves preformed

--- Solution/TestSuite/CubePuzzleTests.cs
+++ Solution/TestSuite/CubePuzzleTests.cs
@@ -29,4 +29,28 @@ public class CubePuzzleTests
         CollectionAssert.AreEqual(expectedFrontFace, state.GetFace(CubeFace.Front));
         CollectionAssert.AreEqual(expectedBackFace, state.GetFace(CubeFace.Back));
     }
+
+    [TestMethod]
+    public void TestResetWithoutAnyMoves()
+    {
+        CubePuzzle puzzle = new CubePuzzle();
+
+        puzzle.Reset();
+        CubeState state = puzzle.GetState();
+
+        int[,] expectedTopFace = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+        int[,] expectedBottomFace = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+        int[,] expectedLeftFace = { { 3, 3, 3 }, { 3, 3, 3 }, { 3, 3, 3 } };
+        int[,] expectedRightFace = { { 4, 4, 4 }, { 4, 4, 4 }, { 4, 4, 4 } };
+        int[,] expectedFrontFace = { { 2, 2, 2 }, { 2, 2, 2 }, { 2, 2, 2 } };
+        int[,] expectedBackFace = { { 5, 5, 5 }, { 5, 5, 5 }, { 5, 5, 5 } };
+
+        CollectionAssert.AreEqual(expectedTopFace, state.GetFace(CubeFace.Top));
+        CollectionAssert.AreEqual(expectedBottomFace, state.GetFace(CubeFace.Bottom));
+        CollectionAssert.AreEqual(expectedLeftFace, state.GetFace(CubeFace.Left));
+        CollectionAssert.AreEqual(expectedRightFace, state.GetFace(CubeFace.Right));
+        CollectionAssert.AreEqual(expectedFrontFace, state.GetFace(CubeFace.Front));
+        CollectionAssert.AreEqual(expectedBackFace, state.GetFace(CubeFace.Back));
+    }
+
 }
