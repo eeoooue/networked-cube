63ca1afc1f0875cef5b23c84e444e5f70f612daf
eeoooue <pdmoorhouse@gmail.com>
2025-05-22 08:26:33 +0100

    removed CubeRotator project

---
+++ Solution/LibCubeIntegration/NetworkingConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCubeIntegration
+{
+    public static class NetworkingConfiguration
+    {
+        private static string BaseAddress = "";
+
+        public static string GetAddressForService(string service)
+        {
+            return "";
+        }
+
+        public static int GetPortForService(string service)
+        {
+            switch (service)
+            {
+                default:
+                    return 0;
+            }
+        }
+    }
+}
