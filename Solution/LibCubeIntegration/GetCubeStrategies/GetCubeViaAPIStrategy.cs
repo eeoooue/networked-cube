dd42118e3a9da55f7be2367b9782231c152f4edd
eeoooue <pdmoorhouse@gmail.com>
2025-05-22 08:49:41 +0100

    GetCubeViaApiStrategy now gets server address from NetworkingConfiguration

--- Solution/LibCubeIntegration/GetCubeStrategies/GetCubeViaAPIStrategy.cs
+++ Solution/LibCubeIntegration/GetCubeStrategies/GetCubeViaAPIStrategy.cs
@@ -3,8 +3,13 @@ using LibNetCube;
 
 public class GetCubeViaApiStrategy : IGetCubeStrategy
 {
-    const string ServerAddress = "http://localhost:5295";
+    private string ServerAddress;
     static readonly HttpClient Client = new();
+
+    public GetCubeViaApiStrategy(string serviceName = "CubeService")
+    {
+        ServerAddress = NetworkingConfiguration.GetAddressForServer(serviceName);
+    }
 
     public async Task<CubeState?> GetCubeStateAsync()
     {
