54a26e61a9ad695ed20e6cac3781b676683e78cb
eeoooue <pdmoorhouse@gmail.com>
2025-05-24 14:23:36 +0100

    added keyboard triggers to cube manipulator

--- Solution/CubeManipulator/MainWindow.xaml.cs
+++ Solution/CubeManipulator/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using LibCubeIntegration.PerformMoveStrategies;
+using System.Windows;
+using System.Windows.Input;
 
 namespace CubeManipulator
 {
@@ -7,11 +9,46 @@ namespace CubeManipulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        MoveViaApiStrategy moveStrategy = new MoveViaApiStrategy("CubeProxy");
+
+        private readonly Dictionary<Key, string> moveKeyMap = new()
+        {
+            { Key.U, "U" },
+            { Key.L, "L" },
+            { Key.B, "B" },
+            { Key.M, "M" },
+            { Key.F, "F" },
+            { Key.R, "R" },
+            { Key.D, "D" },
+            { Key.S, "S" },
+        };
+
         public MainWindow()
         {
             InitializeComponent();
 
             DataContext = new CubeManipulatorViewModel(); //Get ViewModel
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (moveKeyMap.TryGetValue(e.Key, out string? moveFound))
+            {
+                if (moveFound is string move)
+                {
+                    ProcessMove(move);
+                }
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            return;
+        }
+
+        private void ProcessMove(string x)
+        {
+            _ = moveStrategy.PerformMoveAsync(x);
         }
     }
 }
