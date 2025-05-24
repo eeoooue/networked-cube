4ec147a091d17467e6199e377e7457a9ddd2f1d5
eeoooue <pdmoorhouse@gmail.com>
2025-05-24 12:24:17 +0100

    drafted a ResetCubeViaSocketStrategy implementation

---
+++ Solution/LibCubeIntegration/ResetCubeStrategies/ResetCubeViaSocketStrategy.cs
@@ -0,0 +1,24 @@
+﻿using LibCubeIntegration.PerformMoveStrategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCubeIntegration.ResetCubeStrategies
+{
+    public class ResetCubeViaSocketStrategy : IResetCubeStrategy
+    {
+        private MoveViaSocketStrategy MoveStrategy;
+
+        public ResetCubeViaSocketStrategy(string serviceName = "DummyService")
+        {
+            MoveStrategy = new MoveViaSocketStrategy(serviceName);
+        }
+
+        public async Task<bool> ResetCubeAsync()
+        {
+            return await MoveStrategy.PerformMoveAsync("RESET");
+        }
+    }
+}
