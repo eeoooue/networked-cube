485e165457dc71ed9b8e15553af87c9d3ce7df7c
eeoooue <pdmoorhouse@gmail.com>
2025-05-27 18:37:31 +0100

    extended MoveTransactionRepository to support lists of CubeMoves

--- Solution/CubeService/MoveTransactionRepository.cs
+++ Solution/CubeService/MoveTransactionRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CubeService.Models;
 using Dapper;
+using LibNetCube;
 using Microsoft.Data.Sqlite;
 
 
@@ -9,16 +10,37 @@ namespace CubeService
     {
         private const string ConnectionString = "Data Source=cubedatabase.db";
 
-        public void InsertMove(string move)
+        public void InsertMove(CubeMove move)
         {
             using var conn = new SqliteConnection(ConnectionString);
             var sql = "INSERT INTO MoveTransactions (Timestamp, MovePerformed) VALUES (@Timestamp, @MovePerformed)";
             conn.Execute(sql, new
             {
                 Timestamp = DateTime.UtcNow.ToString("o"),
-                MovePerformed = move
+                MovePerformed = move.ToString()
             });
         }
+
+        public void InsertMoves(IEnumerable<CubeMove> moves)
+        {
+            using var conn = new SqliteConnection(ConnectionString);
+            conn.Open(); // required for transaction
+
+            using var transaction = conn.BeginTransaction();
+            var sql = "INSERT INTO MoveTransactions (Timestamp, MovePerformed) VALUES (@Timestamp, @MovePerformed)";
+
+            foreach (var move in moves)
+            {
+                conn.Execute(sql, new
+                {
+                    Timestamp = DateTime.UtcNow.ToString("o"),
+                    MovePerformed = move.ToString()
+                }, transaction);
+            }
+
+            transaction.Commit();
+        }
+
 
         public void ClearAllMoves()
         {
