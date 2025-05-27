11df819f7dee6714320f84f802ff22678efca3b3
eeoooue <pdmoorhouse@gmail.com>
2025-05-27 18:31:34 +0100

    Create CubeGameEngine.cs

---
+++ Solution/CubeService/CubeGameEngine.cs
@@ -0,0 +1,71 @@
+﻿using CubeService.Models;
+using LibNetCube;
+
+namespace CubeService
+{
+    public class CubeGameEngine
+    {
+        private readonly CubePuzzle _cubePuzzle;
+        private readonly MoveTransactionRepository _repo;
+        private readonly object _lock = new();
+
+        public CubeGameEngine(MoveTransactionRepository repo)
+        {
+            _cubePuzzle = new CubePuzzle();
+            _repo = repo;
+            HydrateCubeState();
+        }
+
+        private void HydrateCubeState()
+        {
+            IEnumerable<MoveTransaction> moves = _repo.GetAllMoves();
+            foreach (MoveTransaction move in moves)
+            {
+                if (Enum.TryParse<CubeMove>(move.MovePerformed, out var parsed))
+                {
+                    _cubePuzzle.PerformMove(parsed);
+                }
+            }
+        }
+
+        public void PerformMove(CubeMove move)
+        {
+            lock (_lock)
+            {
+                _cubePuzzle.PerformMove(move);
+                _repo.InsertMove(move.ToString());
+            }
+        }
+
+        public void Reset()
+        {
+            lock (_lock)
+            {
+                _cubePuzzle.Reset();
+                _repo.ClearAllMoves();
+            }
+        }
+
+        public CubeState GetCubeState()
+        {
+            CubeState result;
+            lock (_lock)
+            {
+                result = _cubePuzzle.GetState();
+            }
+
+            return result;
+        }
+
+        public int[,] GetCubeFace(CubeFace face)
+        {
+            int[,] result;
+            lock (_lock)
+            {
+                result = _cubePuzzle.ReadFace(face);
+            }
+
+            return result;
+        }
+    }
+}
